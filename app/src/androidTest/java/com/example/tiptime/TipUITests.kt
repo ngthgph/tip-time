283e8ae4d97afff69bf6b93d9f1e9b101d255ea7
ngthgph <ngthgph112@gmail.com>
2023-07-30 19:23:05 +0700

    added automated tests

---
+++ app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -0,0 +1,33 @@
+package com.example.tiptime
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tiptime.ui.theme.TipTimeTheme
+import org.junit.Rule
+import org.testng.annotations.Test
+import java.text.NumberFormat
+
+class TipUITests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                Surface(modifier = Modifier.fillMaxWidth()) {
+                    TipTimeLayout()
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists("No node with this text was found.")
+    }
+}
--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -171,7 +172,8 @@ fun RoundTheTipRow(
     }
 }
 
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp: Boolean
